d4844327d505c88ec0803af999bfb6f7706005b9
MahiroYamazaki <yamazaki.m.au@m.titech.ac.jp>
2021-05-06 18:21:43 +0900

    とりあえずこれで

--- kadai_gw/kadai_gw.py
+++ kadai_gw/kadai_gw.py
@@ -7,8 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 #追加したimport
 from sklearn.decomposition import TruncatedSVD
-from sklearn.model_selection import RandomizedSearchCV
-import scipy.stats
+import random
 import time
 
 def check_args(args):
@@ -47,7 +46,7 @@ if __name__ == "__main__":
     parser.add_argument("-train", help="path to training data (required)")
     parser.add_argument("-test", help="path to test data (optional)")
     parser.add_argument("-out", help="path to predicted information for test data (required only if --test is set)")
-    parser.add_argument("--window_radius", type=int, default=1)
+    parser.add_argument("--window_radius", type=int, default=10)
     args = parser.parse_args()
 
     check_args(args)
@@ -69,7 +68,7 @@ if __name__ == "__main__":
     window_radius = args.window_radius
     train_data_ = generate_input(train_df, window_radius)
     onehot = OneHotEncoder().fit(train_data_)
-    n = 64
+    n = 460
     pca = TruncatedSVD(n_components=n)
     train_data  = onehot.transform(train_data_)
     del train_data_
@@ -96,58 +95,40 @@ if __name__ == "__main__":
 
     ###### 2. model construction (w/ training dataset) ######
 
-    # lr = LogisticRegression()
-    # model = lr.fit(X_train, y_train)
-
-    max_score = 0
-
-    LR_random = {LogisticRegression(): {"C": scipy.stats.uniform(0.00001, 1000),"random_state": scipy.stats.randint(0, 100)}}
-
-    for model, param in LR_random.items():
-        clf = RandomizedSearchCV(model, param)
-        clf.fit(X_train, y_train)
-        score = model.score(X_val, y_val)
-
-        if max_score < score:
-            max_score = score
-            best_param = clf.best_params_
-            best_model = model.__class__.__name__
-
-    print("ベストスコア:{}".format(max_score))
-    print("モデル:{}".format(best_model))
-    print("パラメーター:{}".format(best_param))
+    lr = LogisticRegression(C=1000)
+    model = lr.fit(X_train, y_train)
 
     ###### 3. model evaluation (w/ validation dataset) ######
 
-    # score = model.score(X_val, y_val)  #デフォルト
-    # auc = roc_auc_score(y_val, model.predict_proba(X_val)[:, 1])  #デフォルト
-    #
-    # print('window_radius: %d'%(window_radius))
-    # print('n: %d'%(n))
-    # print('Q2 accuracy: %.4f'%(score))
-    # print('AUC: %.4f'%(auc))  #デフォルト
+    score = model.score(X_val, y_val)  #デフォルト
+    auc = roc_auc_score(y_val, model.predict_proba(X_val)[:, 1])  #デフォルト
 
-    # ###### 4. prediction for test dataset ######
-    #
-    # if (test_df is not None) and (X_test is not None):  #これ以降で出力の仕方を設定
-    #
-    #     predicted = model.predict_proba(X_test)[:, 1]
-    #
-    #     sequence_id_list    = []
-    #     residue_number_list = []
-    #     for _, item in test_df.iterrows():
-    #         sequence_id = item.sequence_id
-    #         sequence    = item.sequence
-    #         for i, aa in enumerate(sequence):
-    #             sequence_id_list.append(sequence_id)
-    #             residue_number_list.append(i+1) #0-origin to 1-origin
-    #
-    #     predicted_df = pd.DataFrame.from_dict({  #「from_dict」でdataframeにして出力
-    #         "sequence_id": sequence_id_list,
-    #         "residue_number": residue_number_list,
-    #         "predicted_value": predicted,
-    #         })
-    #     predicted_df.to_csv(args.out, index=None)
+    print('window_radius: %d'%(window_radius))
+    print('n: %d'%(n))
+    print('Q2 accuracy: %.4f'%(score))
+    print('AUC: %.4f'%(auc))  #デフォルト
+
+    ###### 4. prediction for test dataset ######
+
+    if (test_df is not None) and (X_test is not None):  #これ以降で出力の仕方を設定
+
+        predicted = model.predict_proba(X_test)[:, 1]
+
+        sequence_id_list    = []
+        residue_number_list = []
+        for _, item in test_df.iterrows():
+            sequence_id = item.sequence_id
+            sequence    = item.sequence
+            for i, aa in enumerate(sequence):
+                sequence_id_list.append(sequence_id)
+                residue_number_list.append(i+1) #0-origin to 1-origin
+
+        predicted_df = pd.DataFrame.from_dict({  #「from_dict」でdataframeにして出力
+            "sequence_id": sequence_id_list,
+            "residue_number": residue_number_list,
+            "predicted_value": predicted,
+            })
+        predicted_df.to_csv(args.out, index=None)
 
     elapsed_time = time.time() - start
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
